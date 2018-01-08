654758f42139037e19b31ddd971fda70c0eed16b
prokr8056 <prokr8056@HRH-IC0027338.wrdsb.ca>
2018-01-08 09:35:15 -0500

    initial commit

---
+++ desktop/src/com/gdxgame/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdxgame.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdxgame.game.ICS4UIgame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ICS4UIgame(), config);
+	}
+}
